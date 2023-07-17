c8e1f5feb9d86c2caae7dcd1742bbbfee6a9b95d
yousan <ka1357amnbpdr@gmail.com>
2023-07-17 16:50:44 +0900

    feat: ルート計算

---
+++ Assets/Scripts/DrawPath.cs
@@ -0,0 +1,31 @@
+﻿using NaughtyAttributes;
+using UnityEngine;
+using UnityEngine.AI;
+
+[DefaultExecutionOrder(10)]
+public class DrawPath : MonoBehaviour
+{
+    [SerializeField] private LineRenderer line;
+
+    [SerializeField] private Transform startPos, endPos;
+
+    private NavMeshPath _path;
+
+    private void Start()
+    {
+        DrawPathLine();
+    }
+
+    [Button("Draw Path")]
+    private void DrawPathLine()
+    {
+        _path = new NavMeshPath();
+        var result = NavMesh.CalculatePath(startPos.position, endPos.position, NavMesh.AllAreas, _path);
+        Debug.Log("Result: " + result + ", Length: " + _path.corners.Length);
+
+        if (!result) return;
+        var corners = _path.corners;
+        line.positionCount = corners.Length;
+        line.SetPositions(corners);
+    }
+}
---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    private NavMeshAgent2D _agent; //NavMeshAgent2Dを使用するための変数
+    [SerializeField] private Transform target; //追跡するターゲット
+
+    private void Start()
+    {
+        _agent = GetComponent<NavMeshAgent2D>(); //agentにNavMeshAgent2Dを取得
+    }
+
+    private void Update()
+    {
+        _agent.destination = target.position; //agentの目的地をtargetの座標にする
+        //agent.SetDestination(target.position); //こっちの書き方でもオッケー
+    }
+}
